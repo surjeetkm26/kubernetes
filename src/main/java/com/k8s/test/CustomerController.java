33761ebb980d32849559309396e84b66ff912c9c
Surjeet Mohanty <surjeetmohanty26@gmail.com>
2020-08-19 21:26:29 +0530

    testes

--- src/main/java/com/k8s/test/CustomerController.java
+++ src/main/java/com/k8s/test/CustomerController.java
@@ -9,18 +9,23 @@ public class CustomerController {
 		System.out.println("Hello Welcome dude");
 		return "Good Morning!";
 	}
+
 	public String getCustomerMessage() {
 		System.out.println("Hello Welcome dude");
 		return "Good Morning!";
 	}
-	public String test(String str,Integer value,Float amount) {
-		if(str!=null) {
+
+	public String test(String str, Integer value, Float amount) {
+		if (str != null) {
 			System.out.println("String not null.");
 		}
-		System.out.println("Hello"+"Jai Shri RAM"+ "Jai Bhole Shankar"+ "Jai Sambhu Baba mere Bhole nath"+ str);
+		System.out.println("Hello" + "Jai Shri RAM" + "Jai Bhole Shankar" + "Jai Sambhu Baba mere Bhole nath" + str);
+		System.out.println("Jai Ganesh! " + "Happy Ganesh Chatuthi");
+		System.out.println("Hello" + "Jai Shri RAM" + "Jai Bhole Shankar");
 		return str;
 	}
-	public static Integer addFuture1(int i,int j) {
-		return i+j;
+
+	public static Integer addFuture1(int i, int j) {
+		return i + j;
 	}
 }
