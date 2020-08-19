51db5e233e8ae7c653b252a43ebae28554c34e07
Surjeet Mohanty <surjeetmohanty26@gmail.com>
2020-08-19 17:30:08 +0530

    Test class created in future 1 branch

---
+++ src/main/java/com/k8s/test/Hello.java
@@ -0,0 +1,8 @@
+package com.k8s.test;
+
+public class Hello {
+
+	public void method1() {
+		System.out.println("I am method 1");
+	}
+}
---
+++ src/main/java/com/k8s/test/TestOne.java
@@ -0,0 +1,5 @@
+package com.k8s.test;
+
+public class TestOne {
+
+}
