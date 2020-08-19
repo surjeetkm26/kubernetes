eadc38a5d1660151bdf46155cbfa69d03ca2f0f0
Surjeet Mohanty <surjeetmohanty26@gmail.com>
2020-08-19 20:19:14 +0530

    Happy Ganesh Chatuthi added Bug Fixed

--- src/main/java/com/k8s/test/CustomerController.java
+++ src/main/java/com/k8s/test/CustomerController.java
@@ -14,7 +14,8 @@ public class CustomerController {
 		return "Good Morning!";
 	}
 	public String test(String str) {
-		System.out.println("Hello"+"Jai Shri RAM"+ "Jai Bhole Shankar");
+	System.out.println("Jai Ganesh! "+ "Happy Ganesh Chatuthi");	
+	System.out.println("Hello"+"Jai Shri RAM"+ "Jai Bhole Shankar");
 		return str;
 	}
 }
