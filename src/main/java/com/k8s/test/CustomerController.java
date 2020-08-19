2db602c1e3718c7e7cf3a877ec5baee90fd1adb4
Surjeet Mohanty <surjeetmohanty26@gmail.com>
2020-08-19 20:47:17 +0530

    Bugggggg fixed

--- src/main/java/com/k8s/test/CustomerController.java
+++ src/main/java/com/k8s/test/CustomerController.java
@@ -9,13 +9,16 @@ public class CustomerController {
 		System.out.println("Hello Welcome dude");
 		return "Good Morning!";
 	}
+
 	public String getCustomerMessage() {
 		System.out.println("Hello Welcome dude");
 		return "Good Morning!";
 	}
 	public String test(String str) {
-	System.out.println("Jai Ganesh! "+ "Happy Ganesh Chatuthi");	
-	System.out.println("Hello"+"Jai Shri RAM"+ "Jai Bhole Shankar");
+		System.out.println("Bug Fixed");
+		System.out.println("Jai Ganesh! " + "Happy Ganesh Chatuthi");
+		System.out.println("Hello" + "Jai Shri RAM" + "Jai Bhole Shankar");
 		return str;
+
 	}
 }
